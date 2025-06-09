5facf6308580e8646408cd5bf3b45ed9a392f742
Humexd-crypto <hugo020395@gmail.com>
2025-06-09 14:48:13 -0600

    2.1

---
+++ base_palabras.js
@@ -0,0 +1,14 @@
+const palabras = [
+  {
+    palabra: "AHORRO",
+    pistas: ["Es un buen hábito financiero", "Sirve para el retiro", "Se hace con dinero"]
+  },
+  {
+    palabra: "COMISION",
+    pistas: ["La cobran las AFORE", "Se calcula sobre el saldo", "Está topada al 0.566%"]
+  },
+  {
+    palabra: "RENTA",
+    pistas: ["Forma de pensión", "Se ajusta por inflación", "Es vitalicia"]
+  }
+];
